22a73b50e031f8101f62c93cd925e02573b10faa
tbn02177@tecban.com <tbn02177@tecban.com>
2019-10-03 12:28:23 -0300

    Reestruturação das Pastas

---
+++ static/js/utils.js
@@ -0,0 +1,27 @@
+
+/*----------------------------------------------Formulário de Cadastro---------------------------------------------*/
+    $(function(){
+        var atual_fs, next_fs, prev_fs;
+        var formulario = $('form[id=cadastro-systems]');
+
+        /*----------------------------------------Barra de progress, próximo----------------------------------------*/
+        $('.next').click(function(){
+            atual_fs = $(this).parent();
+            next_fs = $(this).parent().next();
+
+            $('#progress li').eq($('fieldset').index(next_fs)).addClass('ativo');
+            atual_fs.hide(800);
+            next_fs.show(800);
+        });
+
+        /*----------------------------------------Barra de progress, anterior----------------------------------------*/
+        $('.prev').click(function(){
+            atual_fs = $(this).parent();
+            prev_fs = $(this).parent().prev();
+
+            $('#progress li').eq($('fieldset').index(atual_fs)).removeClass('ativo');
+            atual_fs.hide(800);
+            prev_fs.show(800);
+        });
+
+    });
