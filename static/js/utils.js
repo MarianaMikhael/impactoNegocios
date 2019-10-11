fb0d171882c59ce461b2f9108b44f61b16d90c84
tbn02177@tecban.com <tbn02177@tecban.com>
2019-10-11 13:07:22 -0300

    Função para alternar opacidade do navbar

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,27 +1,37 @@
-
-/*----------------------------------------------Formulário de Cadastro---------------------------------------------*/
-    $(function(){
-        var atual_fs, next_fs, prev_fs;
-        var formulario = $('form[id=cadastro-systems]');
-
-        /*----------------------------------------Barra de progress, próximo*/
-        $('.next').click(function(){
-            atual_fs = $(this).parent();
-            next_fs = $(this).parent().next();
-
-            $('#progress li').eq($('fieldset').index(next_fs)).addClass('ativo');
-            atual_fs.hide(800);
-            next_fs.show(800);
-        });
-
-        /*----------------------------------------Barra de progress, anterior*/
-        $('.prev').click(function(){
-            atual_fs = $(this).parent();
-            prev_fs = $(this).parent().prev();
-
-            $('#progress li').eq($('fieldset').index(atual_fs)).removeClass('ativo');
-            atual_fs.hide(800);
-            prev_fs.show(800);
-        });
-
+/*------------------------------Navbar, transparente => sólido------------------------------*/
+jQuery(function () {
+    jQuery(window).scroll(function () {
+        if (jQuery(this).scrollTop() > 50) {
+            $("#navbar-default").css('background-color', 'rgb(27, 38, 49)');
+        } else {
+            $("#navbar-default").css('background-color', 'rgba(27, 38, 49, 0.7)');
+        }
     });
+});
+
+/*----------------------------------Formulário de Cadastro----------------------------------*/
+$(function(){
+    var atual_fs, next_fs, prev_fs;
+    var formulario = $('form[id=cadastro-systems]');
+
+    /*--------------------Barra de progress, próximo--------------------*/
+    $('.next').click(function(){
+        atual_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+
+        $('#progress li').eq($('fieldset').index(next_fs)).addClass('ativo');
+        atual_fs.hide(800);
+        next_fs.show(800);
+    });
+
+    /*--------------------Barra de progress, anterior--------------------*/
+    $('.prev').click(function(){
+        atual_fs = $(this).parent();
+        prev_fs = $(this).parent().prev();
+
+        $('#progress li').eq($('fieldset').index(atual_fs)).removeClass('ativo');
+        atual_fs.hide(800);
+        prev_fs.show(800);
+    });
+
+});
